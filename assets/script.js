3f7d2a97763a88d7f4adbca2606bc2f8eddc98c5
NGAM TARYLL <taryllngam@gmail.com>
2023-05-05 13:33:38 +0100

    added functionality to displayField with js

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,20 @@ const searchBtn = document.getElementById('button')
 const displayField = document.getElementById('display')
 
 const base_url =`https://api.github.com/users/`;
-let followers_url = "https://api.github.com/users/taryll/followers"
 
 async function searchUser() {
     let userName = user.value
-    const data = await fetch(base_url + userName).then((res) => res.json())
+    const data = await fetch(base_url + userName).then((res) => res.json()).catch((err => 
+        displayField.innerHTML = `NO PROFILE FOUND`
+        ))
+
     console.log(data);
     displayField.innerHTML = `
     <a href="https://github.com/${data.login}" target="_blank">
     <img src="${data.avatar_url}" alt="profile picture"/> 
   </a>
   <div class="following">
-  <h2><a href="https://github.com/${data.login}" target="_blank"> ${data.login}</a></h2>
+  <h2><a href="https://github.com/${data.login}" target="_blank"> ${data.name}</a></h2>
   <p><a href="https://github.com/${data.login}?tab=repositories" target="_blank"> Reposatories: ${data.public_repos}</a></p>
   </div>
   <div class="follows">
@@ -23,4 +25,7 @@ async function searchUser() {
 </div>
 `
 }
+
+
+
 searchBtn.addEventListener("click", searchUser)
